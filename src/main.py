fe5f0ab8a6aae8b081819d27e8fb5e62197029c1
Caleb Rowland <cjrowland4@gmail.com>
2024-03-22 17:51:25 -0500

    Add recursive page generation function

--- src/main.py
+++ src/main.py
@@ -11,10 +11,37 @@ def main() -> None:
     os.mkdir(public)
     copy_contents(static, public)
 
-    from_path = r".\content\index.md"
+    from_dir_path = r".\content"
     template_path = r".\template.html"
-    dest_path = r".\public\index.html"
-    generate_page(from_path, template_path, dest_path)
+    dest_dir_path = r".\public"
+    generate_pages_recursive(from_dir_path, template_path, dest_dir_path)
+
+    return
+
+
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+    for item in os.listdir(dir_path_content):
+        path_to_item = os.path.join(dir_path_content, item)
+        if os.path.isfile(path_to_item) and path_to_item.endswith(".md"):
+            dest_path = os.path.join(dest_dir_path, item)
+            generate_page(path_to_item, template_path, dest_path)
+        elif os.path.isdir(path_to_item):
+            new_dest_dir_path = os.path.join(dest_dir_path, item)
+            generate_pages_recursive(path_to_item, template_path, new_dest_dir_path)
+
+    return
+
+
+def copy_contents(from_dir: str, to_dir: str) -> None:
+    for item in os.listdir(from_dir):
+        path_to_item = os.path.join(from_dir, item)
+        if os.path.isdir(path_to_item):
+            new_to_dir = os.path.join(to_dir, item)
+            os.mkdir(new_to_dir)
+            copy_contents(path_to_item, new_to_dir)
+        else:
+            shutil.copy(path_to_item, to_dir)
+            print(f"Copied: {path_to_item} to {to_dir}")
 
     return
 
@@ -47,20 +74,6 @@ def extract_title(markdown: str) -> str:
             return line.lstrip("# ")
 
     raise Exception(f"Missing single-# from file {markdown}")
-
-
-def copy_contents(from_dir: str, to_dir: str) -> None:
-    for item in os.listdir(from_dir):
-        path_to_item = os.path.join(from_dir, item)
-        if os.path.isdir(path_to_item):
-            new_to_dir = os.path.join(to_dir, item)
-            os.mkdir(new_to_dir)
-            copy_contents(path_to_item, new_to_dir)
-        else:
-            shutil.copy(path_to_item, to_dir)
-            print(f"Copied: {path_to_item} to {to_dir}")
-
-    return
 
 
 if __name__ == "__main__":
